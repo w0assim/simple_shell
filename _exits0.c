ea72f7a7a52f877aa7e3ea641fda919337f0fc78
lamia1998 <lamiaachrih1998@gmail.com>
2023-07-26 17:37:44 -0700

    team project

--- _exits0.c
+++ _exits0.c
@@ -1,53 +1,47 @@
 #include "shell.h"
 
 /**
- *
- *_strchr - The locate of a character in a string
- *@s: string should be parsed
- *
- *@c: character which look for it
- *
- *Return: (s) the pointer of the memory area s
+ * _strchr - The locate of a character in a string
+ * @s: string should be parsed
+ * @c: character which look for it
+ * Return: (s) the pointer of the memory area s
  *
  */
 
-char *_strchr( char *s, char c )
+char *_strchr(char *s, char c)
 {
 	do {
-		if ( *s == c )
+		if (*s == c)
 			return (s);
-	} while ( *s++ != '\0' );
+	} while (*s++ != '\0');
 
 	return (NULL);
 }
 
 /**
- *
- *_strncpy - the copie of string
- *@dest: the destination string should be copied
- *
- *@src: the source of string
- *@n: the amount of characters should be copied
- *
- *Return: the concatenated of string
+ * _strncpy - the copie of string
+ * @dest: the destination string should be copied
+ * @src: the source of string
+ * @n: the amount of characters should be copied
+ * Return: the concatenated of string
  *
  */
 
-char *_strncpy( char *dest, char *src, int n )
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
 
 	i = 0;
-	while ( src[i] != '\0' && i < n - 1 )
+	while (src[i] != '\0' && i < n - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if ( i < n )
+	if (i < n)
 	{
 		j = i;
-		while ( j < n )
+		while (j < n)
 		{
 			dest[j] = '\0';
 			j++;
@@ -57,33 +51,30 @@ char *_strncpy( char *dest, char *src, int n )
 }
 
 /**
- *
- *_strncat - concatenates of two strings
- *@dest: first string
- *@src: second string
- *
- *@n: the amount of a bytes should be maximally used
- *
- *Return: the concatenated strings
+ * _strncat - concatenates of two strings
+ * @dest: first string
+ * @src: second string
+ * @n: the amount of a bytes should be maximally used
+ * Return: the concatenated strings
  *
  */
 
-char *_strncat( char *dest, char *src, int n )
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
 
 	i = 0;
 	j = 0;
-	while ( dest[i] != '\0' )
+	while (dest[i] != '\0')
 		i++;
-	while ( src[j] != '\0' && j < n )
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	if ( j < n )
+	if (j < n)
 		dest[i] = '\0';
 	return (s);
 }
